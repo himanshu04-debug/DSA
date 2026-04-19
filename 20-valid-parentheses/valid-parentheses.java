ebe79dbcd1344d3be5f549e27f2e386deb3a7021
himanshu04-debug <hmidha04@gmail.com>
2026-04-19 18:52:16 +0530

    Time: 3 ms (87.80%) | Memory: 43.2 MB (60.72%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character>st=new Stack<>();
+        int n=s.length();
+        for(int i=0;i<n;i++){
+            char ch=s.charAt(i);
+            if(ch=='('||ch=='{'||ch=='['){
+                st.push(ch);
+            }
+            else{
+                if(st.size()==0)return false;
+                char top=st.pop();
+
+                if ((ch == ')' && top != '(') ||
+                (ch == '}' && top != '{') ||
+                (ch == ']' && top != '[')) {
+                return false;
+            }
+            }
+        }
+            return st.isEmpty();
+
+    }
+}
